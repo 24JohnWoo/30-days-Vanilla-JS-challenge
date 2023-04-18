9f8001e06d29839948d3f001ebc84a45257448d9
John24Woo <eshimi090@gmail.com>
2023-04-17 13:42:42 -1100

    finished js

--- DAY1/app.js
+++ DAY1/app.js
@@ -1,6 +1,22 @@
 window.addEventListener('keydown', (e)=> {
     //console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)// initialises audio to audi key code
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`)// initialises audio to audi key code
+    
+    
     if(!audio) return;  // wont play if the key-code is missing.
+    audio.currentTime = 0;
     audio.play(); 
+    key.classList.add("playing"); 
+
+
 });
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition
+));
